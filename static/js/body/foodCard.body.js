c43875c4ed0e01f6bf18a18411493fce7411733f
jain-hemant <hemantjain1321@gmail.com>
2023-11-14 11:38:22 +0530

    JS Body - foodCard.body,js File Created

---
+++ static/js/body/foodCard.body.js
@@ -0,0 +1,33 @@
+let foodCardImg = document.getElementById("foodCardImg")
+let foodName    = document.getElementById("foodName")
+let cal         = document.getElementById("cal")
+let price       = document.getElementById("price")
+
+let foodCard = [
+    {
+        img: "static/images/product/large-screen/ls1_kfc-8pcSpWings.avif",
+        foodName: "8pc. Wings Combo ",
+        cal: "2800-4400",
+        price: 400,    
+    },    
+    {
+        img: "static/images/product/large-screen/ls2_kfc-FillUpBox.avif",
+        foodName: "Fill Up Box",
+        cal: "2600-3630",
+        price: 600,    
+    },
+    {
+        img: "static/images/product/large-screen/ls4_kfc-6pcCOB12pcNuggets-DigEx.avif",
+        foodName: "Chicken + Nuggets Family Meal ",
+        cal: "2420-4500",
+        price: 800,    
+    },
+    {
+        img: "static/images/product/large-screen/ls5_kfc-8pcBucket&8TendersBucket.avif",
+        foodName: "8 pc. Chicken + 8 Tenders Fill Up ",
+        cal: "5920-6220",
+        price: 1200,    
+    },
+
+]
+
