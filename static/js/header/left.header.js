da3ff37a9872187e6d829723b0a427831d528309
hemantjain1321 <hemantjain1321@gmail.com>
2023-11-16 00:25:11 +0530

    menuDashboard Created with stylesheet need more improvement of menuDashboard.js and menu.html, offerCard Slider still pending to finish

--- static/js/header/left.header.js
+++ static/js/header/left.header.js
@@ -2,7 +2,7 @@ let boxArrow = "https://cdn1.iconfinder.com/data/icons/bootstrap/16/box-arrow-up
 export const leftHeader = `
     <div id="leftHeader">        
         <p id="logo">Kentucky Fried Chicken</p>
-        <a href="">Menu</a>
+        <a href="pages/menu/menu.html">Menu</a>
         <a href="">Careers</a>
         <a href="">Merch</a>
         <a href="">Find A KFC <img src="${boxArrow}" height="12px"></a>
--- static/js/menu/menuDashboard.js
+++ static/js/menu/menuDashboard.js
@@ -1,75 +1,76 @@
-let menuOfferBox = document.getElementById("menuOfferBox")
-
-export function menuCard(){
-    for(menu in menuCardData){
-        let div = document.createElement("div")
-        div.setAttribute("class", "item")
-        let link = document.createElement("a")
-        div.setAttribute("href", "#")
-        let img = menuCardData[menu]["item"]
-        let title = menuCardData[menu]["title"]
-        link.append(img,title)
-        div.append(link)
-        menuOfferBox.innerText = div
-    }
-    return menuOfferBox
-}
-
+let menuOfferBox = document.getElementById("menuCard")
 let menuCardData = [
     {
-        item:"static/images/menu/008tenders/808kfc-new8pcTenders.avif", 
+        item:"/static/images/menu/008tenders/808kfc-new8pcTenders.avif", 
         title:"SPECIAL OFFER"
     }, 
     {
-        item:"static/images/menu/002combos/201kfc-2BWCombo.avif", 
+        item:"/static/images/menu/002combos/201kfc-2BWCombo.avif", 
         title:"COMBOS"
     },
     {
-        item:"static/images/menu/003family_meals/302kfc-8pcBucketMeal.avif", 
+        item:"/static/images/menu/003family_meals/302kfc-8pcBucketMeal.avif", 
         title:"FAMILY MEALS"
     },
     {
-        item:"static/images/menu/004wraps/402kfc-Wraps2for5NEW.avif", 
+        item:"/static/images/menu/004wraps/402kfc-Wraps2for5NEW.avif", 
         title:"WRAPS"
     },
     {
-        item:"static/images/menu/005fried_chicken/503kfc-4pcCombo.avif", 
+        item:"/static/images/menu/005fried_chicken/503kfc-4pcCombo.avif", 
         title:"FRIED CHICKEN"
     },
     {
-        item:"static/images/menu/006nuggets/6003kfc-JamalMurray12pcNugCombo.avif", 
+        item:"/static/images/menu/006nuggets/6003kfc-JamalMurray12pcNugCombo.avif", 
         title:"NUGGETS"
     },
     {
-        item:"static/images/menu/007sandwiches/705kfc-cknsndalc.avif", 
+        item:"/static/images/menu/007sandwiches/705kfc-cknsndalc.avif", 
         title:"SANSWICHES"
     },
     {
-        item:"static/images/menu/008tenders/801kfc-TendersCombo.avif", 
+        item:"/static/images/menu/008tenders/801kfc-TendersCombo.avif", 
         title:"TENDERS"
     },
     {
-        item:"static/images/menu/009pot_pies_bowls/903kfc-FamousBowlNuggetsV2.avif", 
+        item:"/static/images/menu/009pot_pies_bowls/903kfc-FamousBowlNuggetsV2.avif", 
         title:"POT PIES & BOWLS"
     },
     {
-        item:"static/images/menu/010kids_meal/1001kfc-KidsMealNuggetpromoPDP.avif", 
+        item:"/static/images/menu/010kids_meal/1001kfc-KidsMealNuggetpromoPDP.avif", 
         title:"KIDS MEAL"
     },
     {
-        item:"static/images/menu/011sides_add_ons/2002kfc-IndFries.avif", 
+        item:"/static/images/menu/011sides_add_ons/2002kfc-IndFries.avif", 
         title:"SIDE & ADD ONS"
     },
     {
-        item:"static/images/menu/012desserts/3002kfc-CCCake.avif", 
+        item:"/static/images/menu/012desserts/3002kfc-CCCake.avif", 
         title:"DESSERTS"
     },
     {
-        item:"static/images/menu/013sauces/4001kfc-KFCSauce.avif", 
+        item:"/static/images/menu/013sauces/4001kfc-KFCSauce.avif", 
         title:"SAUCES"
     },
     {
-        item:"static/images/menu/014drinks/5001kfc-Pepsi.avif", 
+        item:"/static/images/menu/014drinks/5001kfc-Pepsi.avif", 
         title:"DRINKS"
     },
 ]
+
+for(let menu in menuCardData){
+    let div = document.createElement("div")
+    div.setAttribute("class", "item")
+    let link = document.createElement("a")
+    link.setAttribute("href", "#")
+    let img = document.createElement("img")
+    img.setAttribute("src",menuCardData[menu]["item"])    
+    img.setAttribute("width", "200")
+    let title = document.createElement("h2")
+    title.setAttribute("class", "title")
+    title = menuCardData[menu]["title"] 
+    div.append(img,title)
+    link.append(div)
+    console.log(link);
+    menuCard.append(link)
+}
